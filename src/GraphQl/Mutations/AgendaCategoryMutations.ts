0de7495ad4beadec81afc0c998702ac4e74fed1f
Takshak Mudgal <takshakmudgal@gmail.com>
2024-03-14 00:06:52 +0530

    feat: add GraphQL mutations for agenda categories

---
+++ src/GraphQl/Mutations/AgendaCategoryMutations.ts
@@ -0,0 +1,48 @@
+import gql from 'graphql-tag';
+
+/**
+ * GraphQL mutation to create an agenda category.
+ *
+ * @param input - Name, Description, OrganizationID of the AgendaCategory.
+ */
+
+export const CREATE_AGENDA_CATEGORY_MUTATION = gql`
+  mutation CreateAgendaCategory($input: CreateAgendaCategoryInput!) {
+    createAgendaCategory(input: $input) {
+      _id
+      name
+    }
+  }
+`;
+
+/**
+ * GraphQL mutation to delete an agenda category.
+ *
+ * @param deleteAgendaCategoryId - The ID of the AgendaCategory to be deleted.
+ */
+
+export const DELETE_AGENDA_CATEOGRY_MUTATION = gql`
+  mutation DeleteAgendaCategory($deleteAgendaCategoryId: ID!) {
+    deleteAgendaCategory(id: $deleteAgendaCategoryId)
+  }
+`;
+
+/**
+ * GraphQL mutation to update an agenda category.
+ *
+ * @param updateAgendaCategoryId - The ID of the AgendaCategory to be updated.
+ * @param input - Updated Name, Description, OrganizationID of the AgendaCategory.
+ */
+
+export const UPDATE_AGENDA_CATEGORY_MUTATION = gql`
+  mutation UpdateAgendaCategory(
+    $updateAgendaCategoryId: ID!
+    $input: UpdateAgendaCategoryInput!
+  ) {
+    updateAgendaCategory(id: $updateAgendaCategoryId, input: $input) {
+      _id
+      name
+      description
+    }
+  }
+`;
